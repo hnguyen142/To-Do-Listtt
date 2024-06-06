ea1996999783706400a10c18ee62540e27b9d82d
hnguyen142 <hnguyen142@example.com>
2024-06-06 09:53:41 -0500

    fist commit

---
+++ src/Card.jsx
@@ -0,0 +1,10 @@
+function Card(){
+    return(
+        <div className="card">
+            <img></img>
+            <h2>Bro Code</h2>
+            <p>I make a Youtube video and play video games</p>
+        </div>
+    );
+}
+export default Card
